8fe4a4ea588b8baece91ba45d744e956d78d4db9
nabilahsaleh <nabilahsalzu@gmail.com>
2026-03-27 20:42:09 +0800

    fix: adjust display format

--- src/menu.c
+++ src/menu.c
@@ -4,6 +4,7 @@
 
 void display_menu(int *user_entry)
 {
+    printf("\n==========================================\n");
     printf("\n      WELCOME TO WORD SCRAMBLE GAME!\n");
     printf("\n==========================================\n\n");
     printf("\t1. New Game\n");
@@ -16,7 +17,7 @@ void display_menu(int *user_entry)
 
 void show_help()
 {
-    printf("================== Help ==================\n");
+    printf("================== Help ==================\n\n");
     printf("> Use keyboard to play the game\n");
     printf("> Follow the instruction shown in game New\n");
     printf("> Try to get the highest score\n");
@@ -24,7 +25,7 @@ void show_help()
 
 void exit_game()
 {
-    printf("================== Exit ==================\n");
+    printf("================== Exit ==================\n\n");
     printf("> Thank you for playing\n");
     printf("> The game will Close\n\n");
     exit(0);
